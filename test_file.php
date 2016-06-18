861790ff9c4ba4b81ca7e141ea0a4130943e0ef2
khanburhan <k.khanburhan@gmail.com>
2016-06-19 02:35:42 +0530

    test file added
    This file is for testing purpose only.

---
+++ test_file.php
@@ -0,0 +1,9 @@
+<html>
+<head></head>
+<body>
+	<h1>Test File</h1>
+	<p>This is a test paragraph.This is a test paragraph.This is a test paragraph.This is a test paragraph.
+This is a test paragraph.This is a test paragraph.This is a test paragraph.
+This is a test paragraph.This is a test paragraph.This is a test paragraph.</p>
+</body>
+</html>
